53b470264e54440912e57749b2a892a358d55d57
Tridip Chakraborty <diptridip@gmail.com>
2022-05-01 21:50:51 +0530

    LeetCode!

---
+++ src/com/tridip/exercise/ReerseInteger.java
@@ -0,0 +1,31 @@
+package com.tridip.exercise;
+
+public class ReerseInteger {
+
+	public static int reverse(int x) {
+		
+		
+		
+		return 0;
+		
+		/*String s= "" + x;
+		StringBuilder sb = new StringBuilder(s);
+		s =sb.reverse().toString();
+
+		if(x<0) {
+			s= "-" + s.substring(0, s.length()-1);
+		}
+		try {	
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ne) {
+			return 0;
+		}*/
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(reverse(1534236469));
+	}
+
+}
